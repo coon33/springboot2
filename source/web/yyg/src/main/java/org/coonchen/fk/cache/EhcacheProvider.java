c8f34c42378a4383809aadc577d5055643f9c145
chenwei-2 <56988759@qq.com>
2018-11-23 13:14:08 +0800

    ehcache添加

---
+++ source/web/yyg/src/main/java/org/coonchen/fk/cache/EhcacheProvider.java
@@ -0,0 +1,13 @@
+package org.coonchen.fk.cache;
+
+/**
+ * @ClassName EhcacheProvider
+ * @Description TODO
+ * @Author coonchen
+ * @Date 2018/11/23 0023
+ */
+public class EhcacheProvider {
+
+
+
+}
